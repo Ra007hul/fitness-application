fe2bea0f3eef37917d0f1da37f55ab826be99c0f
unknown <007rahul9254@gmail.com>
2025-10-04 16:28:54 +0530

    Created Activity service end Points and initializes the Eureka server

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,13 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+        RUNNING,
+        CYCLING,
+        SWIMMING,
+        WEIGH_TRAINING,
+        YOGA,
+        HIIT,
+        CARDIO,
+        STRETCHING,
+        OTHER
+}
